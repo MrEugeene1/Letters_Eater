48b7bfeb676ec372b9aa8c529cdc0f0fbe6324cb
MrEugeene1 <eugene-oppong.okyere@student.junia.com>
2026-03-14 18:52:58 +0100

    first commit on python

---
+++ lettersEater.py
@@ -0,0 +1,17 @@
+userWord = input("Enter your Word:")
+userWord = userWord.upper()
+
+
+for letter in userWord:
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    else:
+        print(letter)
